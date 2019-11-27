6c3e14a44a4c3fe9035ebb2220c41df315d38d97
zilc <55707938+zilc@users.noreply.github.com>
2019-11-27 14:11:03 +0200

    Add DemoExecutionCapacitor

---
+++ src/edu/ktu/ds/lab3/cepkauskas/DemoExecutionCapacitor.java
@@ -0,0 +1,27 @@
+package edu.ktu.ds.lab3.cepkauskas;
+
+import edu.ktu.ds.lab3.cepkauskas.ManualTest;
+import edu.ktu.ds.lab3.gui.MainWindowCapacitor;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Locale;
+
+/*
+ * Darbo atlikimo tvarka - čia yra pradinė klasė.
+ */
+public class DemoExecutionCapacitor extends Application {
+
+    public static void main(String[] args) {
+        DemoExecutionCapacitor.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Locale.setDefault(Locale.US); // Suvienodiname skaičių formatus 
+        ManualTest.executeTest();
+        setUserAgentStylesheet(STYLESHEET_MODENA);    //Nauja FX išvaizda
+        //setUserAgentStylesheet(STYLESHEET_CASPIAN); //Sena FX išvaizda
+        MainWindowCapacitor.createAndShowGui(primaryStage);
+    }
+}
